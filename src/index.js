f6311345db46103940d091fa87530e856d6f63e1
Vlad <vladpsico@gmail.com>
2023-01-31 17:15:17 -0600

    cambios a part1

--- src/index.js
+++ src/index.js
@@ -9,30 +9,26 @@ const Title = ({course}) =>{
 const Content1 = ({part1, part2, part3}) =>{
   return <p>{part1} {part2} {part3}</p>
 }
-
+const Exercises = ({exercises1, exercises2, exercises3}) =>{
+  return <p>{exercises1} {exercises2} {exercises3} </p>
+}
 
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <div>
       <Title course={course}/>
-      <Content1 part1={part1 + exercises1}  />
-      <Content1 part2={part2 + exercises2} />
-      <Content1 part3={part3 + exercises3} />
+      <Content1 part1={part1}  /> <Exercises exercises1={exercises1} />
+      <Content1 part2={part2} /> <Exercises exercises2={exercises2} />
+      <Content1 part3={part3} /> <Exercises exercises3={exercises3} />
       
        
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
